f4b3a332a5972cbe97bea4b692e88646576f67df
Nathan Lima <nathanls.dev@gmail.com>
2024-05-09 10:55:29 -0300

    refactor: changed way to get api data to simulate a delay loading (mock)

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,27 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
-    todos: []
+    todos: [],
   },
   mutations: {
     storeTodos(state, payload) {
-      state.todos = payload
-    }
+      state.todos = payload;
+    },
   },
-  getters: {
-  },
+  getters: {},
   actions: {
+    getTodos({ commit }) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          return axios.get("http://localhost:3000/todos").then((response) => {
+            commit("storeTodos", response.data);
+            resolve();
+          });
+        }, 800);
+      });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
