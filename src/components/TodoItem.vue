790d080ca5f88d908f299cd103fc04222565f174
Nathan Lima <nathanls.dev@gmail.com>
2024-05-09 18:32:05 -0300

    feat: implemented functionality to check todo as completed

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -4,7 +4,13 @@
       class="flex items-center px-4 py-3 border-b border-gray-400 last:border-b-0"
     >
       <div class="flex items-center justify-center mr-2">
-        <button class="text-gray-400">
+        <button
+          :class="{
+            'text-green-600': isCompleted,
+            'text-gray-400': isCompleted,
+          }"
+          @click="onCheckClick"
+        >
           <svg
             class="w-5 h-5"
             fill="none"
@@ -24,11 +30,11 @@
 
       <div class="w-full">
         <input
+          v-model="title"
           type="text"
           placeholder="Digite a sua tarefa"
-          :value="todo.title"
           class="bg-gray-300 placeholder-gray-500 text-gray-700 font-light focus:outline-none block w-full appearance-none leading-normal mr-3"
-          @keyup.enter="updateTodo"
+          @keyup.enter="onTitleChange"
         />
       </div>
 
@@ -65,22 +71,39 @@ export default {
       default: () => ({}),
     },
   },
-  methods: {
-    updateTodo({ target }) {
-      const newTitle = target.value.trim();
 
-      if (!newTitle) {
+  data() {
+    return {
+      title: this.todo.title,
+      isCompleted: this.todo.completed,
+    };
+  },
+
+  methods: {
+    onTitleChange({ target }) {
+      this.title = target.value.trim();
+
+      if (!this.title) {
         return;
       }
 
+      this.updateTodo();
+    },
+
+    updateTodo() {
       const payload = {
         id: this.todo.id,
         data: {
-          title: newTitle,
-          completed: this.todo.completed,
+          title: this.title,
+          completed: this.isCompleted,
         },
       };
       this.$store.dispatch("updateTodo", payload);
+    },
+
+    onCheckClick() {
+      this.isCompleted = !this.isCompleted;
+      this.updateTodo();
     },
   },
 };
