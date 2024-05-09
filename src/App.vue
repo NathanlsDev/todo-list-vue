527c5799f0de10e7a1ed0e4498404b9d3a301d68
Nathan Lima <nathanls.dev@gmail.com>
2024-05-08 22:43:01 -0300

    feat: added todo list base template

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
-<script>
-
-</script>
-
 <template>
-
+  <body class="bg-gray-800">
+    <div class="px-3 py-10 md:px-10">
+      <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+        <TodoSpinner />
+        <TodoFormAdd />
+        <TodoItems />
+        <TodoEmpty />
+      </div>
+    </div>
+  </body>
 </template>
 
-<style>
+<script>
+import TodoSpinner from "./components/TodoSpinner.vue";
+import TodoFormAdd from "./components/TodoFormAdd.vue";
+import TodoItems from "./components/TodoItems.vue";
+import TodoEmpty from "./components/TodoEmpty.vue";
 
-</style>
+export default {
+  components: {
+    TodoSpinner,
+    TodoFormAdd,
+    TodoItems,
+    TodoEmpty,
+  },
+};
+</script>
---
+++ src/components/TodoSpinner.vue
@@ -0,0 +1,10 @@
+<template>
+  <figure class="text-center">
+    <img
+      src="@/assets/img/spinner.svg"
+      alt="loading spinner image"
+      class="inline-block w-5 h-5"
+    />
+  </figure>
+</template>
+
